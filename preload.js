b007a7dfd6542287ab558c5ac38082e894a23ede
Firok <firok@qq.com>
2023-04-10 17:17:19 +0800

    init commit

---
+++ preload.js
@@ -0,0 +1,65 @@
+/*
+数据库结构
+
+conf-default-gear-id :string
+conf-params :object
+    {key} :{value}
+gear-{id} :object
+    id :string
+    icon :string
+    name :string
+    pattern :string
+*/
+
+const KeyDefaultGearId = 'conf-default-gear-id'
+const KeyParams = 'conf-params'
+const PrefGearId = 'gear-'
+
+const dbGet = utools.db.get
+const dbGetAll = utools.db.allDocs
+const dbPut = utools.db.put
+const dbRemove = utools.db.remove
+
+const FUNCTIONS = {
+    getDefaultGearId()
+    {
+        return dbGet(KeyDefaultGearId).data
+    },
+    getDefaultGear()
+    {
+        const defaultGearId = FUNCTIONS.getDefaultGearId()
+    },
+    setDefaultGearId(gearId)
+    {},
+    insertGear()
+    {
+    },
+    deleteGear(gearId)
+    {
+        return dbRemove(gearId)
+    },
+    updateGear(gearId, {icon, name, pattern})
+    {
+        return dbPut({
+            _id: gearId,
+            _rev: "1-05c9b92e6f24287dc1f4ec79d9a34fa8",
+            data: {
+                id: gearId,
+                icon,
+                name,
+                pattern,
+            },
+        })
+    },
+    selectGear(gearId)
+    {
+        return dbGet(gearId).data
+    },
+    selectGears()
+    {
+        return dbGetAll(PrefGearId)
+    },
+}
+for(const key in FUNCTIONS)
+    window[key] = FUNCTIONS[key]
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Darmstadtium Gear 𫟼齿轮
+
+> 𫟼 Darmstadtium, Ds, Direct Search
+
+一个用于快速打开搜索引擎网页的 u.tools 插件
